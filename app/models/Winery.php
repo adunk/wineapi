05937bc7da9c71b2f8bc21f6dc882626f15c73a8
Andrew Dunkle <andrew.dunkle@me.com>
2014-05-05 21:43:35 -0700

    Misc. updates.

--- app/models/Winery.php
+++ app/models/Winery.php
@@ -1,5 +1,12 @@
 <?php
 
 class Winery extends \Eloquent {
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'wineries';
+	
 	protected $fillable = [];
 }
